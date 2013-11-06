446db18e30c0abb0a059ec213a6c7342f3220e69
unknown <Todd.Branchflower@DFEC-MXL2181G6J.usafa.ds.af.edu>
2013-11-06 16:01:10 -0700

    updates to gitignore, game_shell, main

--- main.c
+++ main.c
@@ -1,6 +1,6 @@
 #include <msp430.h>
 #include "game_shell/game.h"
-#include "button/buttons.h"
+#include "buttons/button.h"
 #include "LCD/LCD.h"
 
 void init_timer();
@@ -47,6 +47,21 @@ int main(void)
 void init_timer()
 {
 	// do timer initialization work
+
+    TACTL &= ~(MC1|MC0);        // stop timer
+
+    TACTL |= TACLR;             // clear TAR
+
+    TACTL |= TASSEL1;           // configure for SMCLK - what's the frequency (roughly)?
+
+    TACTL |= ID1|ID0;           // divide clock by 8 - what's the frequency of interrupt?
+
+    TACTL &= ~TAIFG;            // clear interrupt flag
+
+    TACTL |= MC1;               // set count mode to continuous
+
+    TACTL |= TAIE;              // enable interrupt
+
 }
 
 void init_buttons()
